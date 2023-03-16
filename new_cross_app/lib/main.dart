d43c063c17437cd6a297fd183d01520b9ecdf472
histoire99ceobe <106070601+histoire99ceobe@users.noreply.github.com>
2023-03-16 23:00:00 +1100

    Set up for Firebase
    Signed-off-by: Hao Ni <nhchristine@outlook.com>

--- new_cross_app/lib/main.dart
+++ new_cross_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -112,4 +113,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
 }
+
+
