200eebb1853b75d587c50e03365511464e090604
histoire99ceobe <106070601+histoire99ceobe@users.noreply.github.com>
2023-03-22 00:27:37 +1100

    Delete Firebase related file.
    Signed-off-by: Hao Ni <nhchristine@outlook.com>

--- new_cross_app/lib/main.dart
+++ new_cross_app/lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:new_cross_app/Calendar/Consumer/ConsumerProfilePage.dart';
-import 'firebase_options.dart';
-import 'package:syncfusion_flutter_calendar/calendar.dart';
 void main() {
   runApp(const MyApp());
 }
