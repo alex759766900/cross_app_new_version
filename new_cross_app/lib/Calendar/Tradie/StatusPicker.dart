4c0a73924d831a9138ec289cff1521e20c26b843
chappiewu <37008392+chappiewuzefan@users.noreply.github.com>
2023-03-28 21:52:52 +1100

    finish part of the tradie's calendar

---
+++ new_cross_app/lib/Calendar/Tradie/StatusPicker.dart
@@ -0,0 +1,44 @@
+part of booking_Calendar;
+
+class _ColorPicker extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ColorPickerState();
+  }
+}
+
+class _ColorPickerState extends State<_ColorPicker> {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: Container(
+          width: double.maxFinite,
+          child: ListView.builder(
+            padding: const EdgeInsets.all(0),
+            itemCount: _statusNames.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                contentPadding: const EdgeInsets.all(0),
+                leading: Icon(
+                    index == _selectedStatusIndex
+                        ? Icons.lens
+                        : Icons.trip_origin,
+                    color: _colorCollection[index]),
+                title: Text(_statusNames[index]),
+                onTap: () {
+                  setState(() {
+                    _selectedStatusIndex = index;
+                  });
+
+                  // ignore: always_specify_types
+                  Future.delayed(const Duration(milliseconds: 200), () {
+                    // When task is over, close the dialog
+                    Navigator.pop(context);
+                  });
+                },
+              );
+            },
+          )),
+    );
+  }
+}
