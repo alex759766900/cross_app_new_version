3e92f601002c53e1931be272130623729df0fcaa
YoumingLiu1 <liuyouming92@gmail.com>
2023-04-12 11:36:15 +1000

    stripe .env/mainStripe2.dart

--- new_cross_app/lib/stripe/constains.dart
+++ new_cross_app/lib/stripe/constains.dart
@@ -3,3 +3,6 @@ import 'package:flutter/material.dart';
 const Color primaryColor = Color(0xFF4CAF50);
 
 const double defaultPadding = 16.0;
+
+const stripePublishableKey =
+    "pk_live_51LhNj9KiAdEOaeSXAsozHoGTrURr1Q5beCaHdryVJOLlsCnBFjz3ug4pOwO6mu2W1snMlvh9jFW5xFDN5fJpi8Dt00q3WwRGXi";
---
+++ new_cross_app/lib/stripe/screens/mainStripe2.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+import 'package:new_cross_app/stripe/screens/card_form_screen.dart';
+import 'package:new_cross_app/stripe/screens/.env';
+import 'package:new_cross_app/stripe/screens/screens.dart';
+import 'package:new_cross_app/stripe/constains.dart';
+import 'package:stripe/stripe.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // TODO
+  Stripe.publishableKey = stripePublishableKey;
+  await Stripe.instance.applySetting();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Jemma Australia',
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+            primary: Color(0xFF000A1F), secondary: Color(0xFF000A1F)),
+        // primarySwatch: Colors.green,
+        primaryColor: Colors.white,
+      ),
+      home: const CardFormScreen(),
+      // HomeScreen,
+    );
+  }
+}
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+//   final String title;
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+//
+// Consumer consumer = Consumer('Lance');
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Home Page"),
+//       ),
+//       //TODO:
+//       // body: ,
+//       drawer: Drawer(
+//         child: ListView(
+//           padding: EdgeInsets.zero,
+//           children: [
+//             const DrawerHeader(
+//               decoration: BoxDecoration(
+//                 color: Colors.lightGreen,
+//               ),
+//               child: Text('Menu'),
+//             ),
+//             ListTile(
+//               title: const Text('Consumer Calendar'),
+//               onTap: () {
+//                 Navigator.push(
+//                     context,
+//                     MaterialPageRoute(
+//                         builder: (context) =>
+//                             ConsumerProfilePage(consumer: consumer)));
+//               },
+//             ),
+//             ListTile(
+//               title: const Text('Tradie Selection'),
+//               onTap: () {
+//                 Navigator.push(
+//                     context,
+//                     MaterialPageRoute(
+//                         builder: (context) => TradieDemo(
+//                               consumer: consumer,
+//                             )));
+//               },
+//             ),
+//             // Stripe CardPayment
+//             ListTile(
+//               title: const Text('Stripe Payment'),
+//               onTap: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => CardFormScreen()),
+//                 );
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
