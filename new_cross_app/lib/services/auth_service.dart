af210f8bbfe4693eef0a7c59ba8d1af0cc75a1ff
lycshmee <547810835@qq.com>
2023-08-24 01:43:33 +1000

    google signin(need to fix database)

--- new_cross_app/lib/services/auth_service.dart
+++ new_cross_app/lib/services/auth_service.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:logger/logger.dart';
 import 'package:new_cross_app/helper/helper_function.dart';
 import 'package:new_cross_app/services/database_service.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
@@ -41,26 +43,39 @@ class AuthService {
     }
   }
 
-  // TODO：更新新的tradie注册
-  /*
-  //sign up for trade person uesr
-  Future registerTradepersonWithEmailAndPassword(
-      String fullName, String email, String password) async {
-    try {
-      User user = (await firebaseAuth.createUserWithEmailAndPassword(
-              email: email, password: password))
-          .user!;
+  // Sign in with Google
+  Future<bool> signInWithGoogle() async {
+    // Create an instance of the firebase auth and google signin
+    FirebaseAuth auth = FirebaseAuth.instance;
+    final GoogleSignIn googleSignIn = GoogleSignIn();
 
-      if (user != null) {
-        //call out database to update the user data
-        await DatabaseService(uid: user.uid).savingTradieData(fullName, email);
-        return true;
+    try {
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+
+      // If the user cancels the Google Sign-In process, googleUser will be null.
+      if (googleUser != null) {
+        // Obtain the auth details from the request
+        final GoogleSignInAuthentication googleAuth =
+            await googleUser.authentication;
+
+        // Create a new credentials
+        final AuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+
+        // Sign in the user with the credentials
+        await auth.signInWithCredential(credential);
+
+        return true; // Return true on successful authentication
       }
-    } on FirebaseAuthException catch (e) {
-      return e.message;
+    } catch (error) {
+      // Handle any errors that might occur during the sign-in process
+      print("Error during Google Sign-In: $error");
     }
+    return false; // Return false if authentication fails or is cancelled
   }
-  */
 
   // signout
   Future signOut() async {
